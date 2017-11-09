9f644f973e79b017f743307f8f861e04f6735d62
IrinaKomen <I.Komen@tudelft.nl>
2017-11-09 13:23:42 +0100

    I added a line to script.py

--- script.py
+++ script.py
@@ -1,1 +1,5 @@
 print('This is a program that is called script.py and just prints a message on the screen')
+
+import test
+
+print('I now want to calculate the circumference of a circle with a radius of 5')
